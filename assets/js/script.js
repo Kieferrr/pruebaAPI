e08720c6d36528a9059795ee1c3e534e42d7b793
Nicolás <nicog.rivero@gmail.com>
2025-05-30 17:06:46 -0400

    Agregar variables iniciales

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const clpInput = document.getElementById("clp-input")
+const currencySelect = document.getElementById("currency-select")
+const converterButton = document.getElementById("converter-button")
+const resultP = document.getElementById("result-p")
