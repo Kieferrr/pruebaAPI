f30759b0d4e29aae331195585f3707df2b7e5b11
Nicolás <nicog.rivero@gmail.com>
2025-05-30 19:24:11 -0400

    Agregar lógica preliminar de obtener monedas en el select

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,24 @@
-const clpInput = document.getElementById("clp-input")
-const currencySelect = document.getElementById("currency-select")
-const converterButton = document.getElementById("converter-button")
-const resultP = document.getElementById("result-p")
+const clpInput = document.getElementById("clp-input");
+const currencySelect = document.getElementById("currency-select");
+const converterButton = document.getElementById("converter-button");
+const resultP = document.getElementById("result-p");
+
+async function obtenerMonedas() {
+    const res = await fetch("https://mindicador.cl/api/");
+    const data = await res.json();
+    
+    const monedas = [
+        data.dolar,
+        data.euro,
+        data.uf,
+    ]
+    
+    for (let moneda of monedas) {
+        const opciones = document.createElement("option")
+        opciones.textContent = moneda.codigo
+        currencySelect.appendChild(opciones)
+    }
+    
+}
+
+obtenerMonedas()
