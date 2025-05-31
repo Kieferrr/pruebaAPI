ee82d1bc947af1ce1a4ca644496fd7593a492e58
Nicolás <nicog.rivero@gmail.com>
2025-05-30 21:30:21 -0400

    Mejorar funcionamiento del gráfico. Ahora se elimina el anterior al crear uno nuevo
    Agregar ; correspondientes y format document

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,35 +3,35 @@ const currencySelect = document.getElementById("currency-select");
 const converterButton = document.getElementById("converter-button");
 const resultP = document.getElementById("result-p");
 
-    // Arreglo para guardar las monedas que llegan desde la API
+// Arreglo para guardar las monedas que llegan desde la API
 
-    let monedas = [];
-    
+let monedas = [];
+
 // Función para llamar a la API y obtener los datos de las divisas
 async function obtenerMonedas() {
     try {
         const res = await fetch("https://mindicador.cl/api/");
         const data = await res.json();
-        
+
         // Guardar solo monedas necesarias
         monedas = [
             data.dolar,
             data.euro,
             data.uf,
         ];
-        
+
         // Crear las opciones del select usando los codigos de la API
         for (let moneda of monedas) {
             const opciones = document.createElement("option");
             opciones.value = moneda.codigo;
             opciones.textContent = moneda.codigo;
             currencySelect.appendChild(opciones);
-        };
+        }
     }
     catch (error) {
         console.log(error);
     };
-};
+}
 
 //Ejecutar la función al cargar la página
 obtenerMonedas();
@@ -49,30 +49,30 @@ converterButton.addEventListener("click", () => {
     //Mostrar resultado en la página
     resultP.innerHTML = conversion.toFixed(2);
 
-    renderGrafica(monedaSeleccionada.codigo)
+    renderGrafica(monedaSeleccionada.codigo);
 });
 
 // Función para llamar a la API y obtener los datos de la variación los últimos 10 días
 
 async function obtenerVariación(moneda) {
     try {
-        const res = await fetch(`https://mindicador.cl/api/${moneda}`)
-        const resultado = await res.json()
+        const res = await fetch(`https://mindicador.cl/api/${moneda}`);
+        const resultado = await res.json();
 
         // Solo últimos 10 días
-        const ultimosDiezDias = resultado.serie.slice(0,10)
+        const ultimosDiezDias = resultado.serie.slice(0, 10);
 
         // Labels para eje X
-        const labels = ultimosDiezDias.map((dia)=> {
+        const labels = ultimosDiezDias.map((dia) => {
             return dia.fecha
-        })
+        });
 
         // Valores para el eje Y
-        const data = ultimosDiezDias.map ((dia) => {
-            const valor = dia.valor
-            return valor
-        })
-        
+        const data = ultimosDiezDias.map((dia) => {
+            const valor = dia.valor;
+            return valor;
+        });
+
         const datasets = [
             {
                 label: "Valor",
@@ -80,23 +80,33 @@ async function obtenerVariación(moneda) {
                 data,
             }
         ]
-        return {labels, datasets}
+        return { labels, datasets };
     }
     catch (error) {
         console.log(error);
-        
+
     }
 }
 
+// Variable global para el grafico actual. Sin valor
+let graficoActual = null;
+
 // Funcion para renderizar el gráfico
 async function renderGrafica(moneda) {
-    const data = await obtenerVariación(moneda)
-    
+    const data = await obtenerVariación(moneda);
+
     const config = {
         type: "line",
         data
+    };
+    const myChart = document.getElementById("myChart");
+    myChart.style.backgroundColor = "white";
+
+    // Si ya hay un gráfico, se borra antes de crear uno nuevo
+    if (graficoActual) {
+        graficoActual.destroy();
     }
-    const myChart = document.getElementById("myChart")
-    myChart.style.backgroundColor = "white"
-    new Chart(myChart, config)
+
+    // Crear y guardar el nuevo gráfico
+    graficoActual = new Chart(myChart, config);
 }
