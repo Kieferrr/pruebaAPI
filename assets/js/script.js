38f1592bdf7d13fdd3a4eb4cbf18a8c423843f8c
Nicolás <nicog.rivero@gmail.com>
2025-05-30 20:03:51 -0400

    Mejorar función obtenerMonedas
    Añadir anotaciones útiles
    
    Agregar onclick
    
    Guardar datos de la API.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,49 @@ const currencySelect = document.getElementById("currency-select");
 const converterButton = document.getElementById("converter-button");
 const resultP = document.getElementById("result-p");
 
-async function obtenerMonedas() {
-    const res = await fetch("https://mindicador.cl/api/");
-    const data = await res.json();
-    
-    const monedas = [
-        data.dolar,
-        data.euro,
-        data.uf,
-    ]
-    
-    for (let moneda of monedas) {
-        const opciones = document.createElement("option")
-        opciones.textContent = moneda.codigo
-        currencySelect.appendChild(opciones)
-    }
-    
-}
+    // Arreglo para guardar las monedas que llegan desde la API
 
-obtenerMonedas()
+    let monedas = [];
+    
+// Función para llamar a la API y obtener los datos de las divisas
+async function obtenerMonedas() {
+    try {
+        const res = await fetch("https://mindicador.cl/api/");
+        const data = await res.json();
+        
+        // Guardar solo monedas necesarias
+        monedas = [
+            data.dolar,
+            data.euro,
+            data.uf,
+        ];
+        
+        // Crear las opciones del select usando los codigos de la API
+        for (let moneda of monedas) {
+            const opciones = document.createElement("option");
+            opciones.value = moneda.codigo;
+            opciones.textContent = moneda.codigo;
+            currencySelect.appendChild(opciones);
+        };
+    }
+    catch (error) {
+        console.log(error);
+    };
+};
+
+//Ejecutar la función al cargar la página
+obtenerMonedas();
+
+// Onclick para hacer la conversion
+converterButton.addEventListener("click", () => {
+
+    //Buscar la moneda seleccionada en el select
+    const monedaSeleccionada = monedas.find(moneda => moneda.codigo === currencySelect.value);
+    const valor = monedaSeleccionada.valor;
+
+    //Calcular la conversión desde CLP
+    const conversion = clpInput.value / valor;
+
+    //Mostrar resultado en la página
+    resultP.innerHTML = conversion.toFixed(2);
+});
